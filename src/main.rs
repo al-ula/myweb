5356e4a2b69ef0b13d7f663622f959e37ffa89e6
Isa Al Ula <106462274+al-ula@users.noreply.github.com>
2024-07-04 19:13:44 +0700

    refactor unwrap

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod post;
 
+use std::path::PathBuf;
+
 use figment::{
     providers::{Env, Format, Serialized, Toml},
     Figment, Profile,
@@ -9,7 +11,7 @@ use post::Html;
 use rocket::{
     fairing::AdHoc,
     get,
-    response::{content::RawHtml, Redirect},
+    response::{content::RawHtml, status::NotFound, Redirect},
     routes, State,
 };
 use serde::{Deserialize, Serialize};
@@ -57,11 +59,21 @@ fn make_data(menus: Vec<Menu>, title: &str) -> Map<String, Value> {
     data
 }
 
-async fn make_page(
-    template_path: &str,
+fn make_page(
+    config: &State<Config>,
     page_name: &str,
     data: Map<String, Value>,
 ) -> Result<post::Html, Box<dyn std::error::Error>> {
+    let template_path = match config
+        .theme_dir
+        .to_owned()
+        .join(&config.theme)
+        .join("templates")
+        .to_str()
+    {
+        Some(t) => t.to_owned(),
+        None => return Err("Failed to get template path".into()),
+    };
     let page_template = format!("{}/{}.hbs", template_path, page_name);
     let mut handlebars = Handlebars::new();
     handlebars
@@ -80,7 +92,7 @@ async fn make_page(
 }
 
 #[get("/")]
-async fn index(config: &State<Config>) -> RawHtml<String> {
+async fn index(config: &State<Config>) -> Result<RawHtml<String>, NotFound<String>> {
     let menus: Vec<Menu> = vec![
         Menu {
             name: "Blog".to_string(),
@@ -96,14 +108,18 @@ async fn index(config: &State<Config>) -> RawHtml<String> {
         },
     ];
     let data = make_data(menus, "ISAALULA");
-    let template_path = format!("theme/{}/templates", &config.theme);
-    println!("Template path: {}", template_path);
-    let html = make_page(&template_path, "index", data).await.unwrap();
-    RawHtml(html.to_string())
+    let html = make_page(config, "index", data);
+    match html {
+        Ok(html) => Ok(RawHtml(html.to_string())),
+        Err(e) => Err(NotFound(e.to_string())),
+    }
 }
 
 #[get("/<page>")]
-async fn page(page: String, config: &State<Config>) -> Result<RawHtml<String>, Redirect> {
+async fn page(
+    page: &str,
+    config: &State<Config>,
+) -> Result<Result<RawHtml<String>, Redirect>, NotFound<String>> {
     let menus: Vec<Menu> = vec![
         Menu {
             name: "Blog".to_string(),
@@ -119,27 +135,25 @@ async fn page(page: String, config: &State<Config>) -> Result<RawHtml<String>, R
         },
     ];
     let data = make_data(menus, &page.to_uppercase());
-    let template_path = format!("theme/{}/templates", &config.theme);
     let page_title: [&str; 3] = ["blog", "projects", "about"];
-    if let Some(title) = page_title.iter().find(|&&t| page == t) {
-        let html = RawHtml(
-            make_page(&template_path, title, data)
-                .await
-                .unwrap_or_default()
-                .to_string(),
-        );
-        return Ok(html);
-    }
-    match page.as_str() {
-        "index" => Err(Redirect::to("/")),
-        _ => Ok(RawHtml(
-            Html::new("404 page not found".to_string()).to_string(),
-        )),
+    match page {
+        "index" => Ok(Err(Redirect::to("/"))),
+        p if page_title.contains(&p) => {
+            let html = make_page(config, p, data);
+            match html {
+                Ok(html) => Ok(Ok(RawHtml(html.to_string()))),
+                Err(e) => Err(NotFound(e.to_string())),
+            }
+        }
+        _ => Err(NotFound("Page not found".to_string())),
     }
 }
 
 #[get("/blog/<blog_post>")]
-async fn blog(blog_post: String, config: &State<Config>) -> Result<RawHtml<String>, Redirect> {
+async fn blog(
+    blog_post: &str,
+    config: &State<Config>,
+) -> Result<RawHtml<String>, NotFound<String>> {
     let menus: Vec<Menu> = vec![
         Menu {
             name: "Blog".to_string(),
@@ -157,29 +171,73 @@ async fn blog(blog_post: String, config: &State<Config>) -> Result<RawHtml<Strin
     let blog_content = fs::read_to_string(format!("articles/blog/{}.md", blog_post))
         .await
         .expect("Failed to read blog content");
-    let html = post::Markdown::new(blog_content)
-        .to_html(post::MarkdownType::Common)
-        .unwrap()
-        .to_string();
-    let mut data = make_data(menus, "Post");
-    data.insert("blog_post".to_string(), to_json(&html));
-    let template_path = format!("theme/{}/templates", &config.theme);
-    let html = make_page(&template_path, "blog_post", data)
-        .await
-        .unwrap_or_default()
-        .to_string();
-    Ok(RawHtml(html))
+
+    let html = match post::Markdown::new(blog_content).to_html(post::MarkdownType::Common) {
+        Ok(h) => h.to_string(),
+        Err(e) => return Err(NotFound(e.to_string())),
+    };
+    let mut data = make_data(menus, &blog_post.snake_to_title_case());
+    data.insert("blog_post".to_string(), to_json(html));
+    let html = make_page(config, "blog_post", data);
+    match html {
+        Ok(html) => Ok(RawHtml(html.to_string())),
+        Err(e) => Err(NotFound(e.to_string())),
+    }
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Config {
     theme: String,
+    theme_dir: PathBuf,
 }
 
 impl Default for Config {
     fn default() -> Config {
+        use std::env;
+        let home_dir: PathBuf = if cfg!(target_os = "windows") {
+            env::var("USERPROFILE").or_else(|_| {
+                let drive = env::var("HOMEDRIVE").expect("HOMEDRIVE environment variable not set");
+                let path = env::var("HOMEPATH").expect("HOMEPATH environment variable not set");
+                Ok(format!("{}{}", drive, path))
+            })
+        } else {
+            env::var("HOME")
+        }
+        .expect("Home directory environment variable not set")
+        .into();
+
+        let theme_dir = home_dir.join("my_web/theme");
+
         Config {
             theme: "default".to_string(),
+            theme_dir,
         }
+    }
+}
+
+trait SnakeToTitleCase {
+    fn snake_to_title_case(&self) -> String;
+}
+
+impl SnakeToTitleCase for &str {
+    fn snake_to_title_case(&self) -> String {
+        fn capitalize_first_letter(s: &str) -> String {
+            let mut c = s.chars();
+            match c.next() {
+                None => String::new(),
+                Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
+            }
+        }
+
+        self.split('_')
+            .map(capitalize_first_letter)
+            .collect::<Vec<_>>()
+            .join(" ")
+    }
+}
+
+impl SnakeToTitleCase for String {
+    fn snake_to_title_case(&self) -> String {
+        self.as_str().snake_to_title_case()
     }
 }
